2397dc89c47793760ab931484a9ee3f3dc09dcc8
TheScripterX <hamdounwassim94@gmail.com>
2021-07-08 23:49:51 +0000

    V 0.2 Added ("Material Sidenav / Navlist / Toolbar / Icons") & Material module configuration

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+//
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroComponent } from './pages/hero/hero.component';
@@ -8,7 +8,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 //
 import { HeroesRoutingModule } from './heroes-routing.module';
-
+import { MaterialModule } from '../material/material.module';
+//
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AddComponent,
@@ -17,6 +19,11 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     HomeComponent,
     ListComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
+    HeroesRoutingModule,
+  ],
 })
 export class HeroesModule {}
