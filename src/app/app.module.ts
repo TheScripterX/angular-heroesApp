b35732c14d8f3699fb37a87aa110d90e71b80acf
TheScripterX <hamdounwassim94@gmail.com>
2021-07-09 00:36:51 +0000

    V 0.2.1 Added HeroesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +11,12 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [AppComponent, ErrorPageComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
